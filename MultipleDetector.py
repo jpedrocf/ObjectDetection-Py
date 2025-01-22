b6fd7b677dab210e3259e5822cd5f8c2abcd8a5f
João Pedro Faria <105388545+jpedrocf@users.noreply.github.com>
2025-01-22 12:08:57 -0300

    Update MultipleDetector.py

--- MultipleDetector.py
+++ MultipleDetector.py
@@ -3,8 +3,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-map_img = cv.imread('https://github.com/jpedrocf/MachineLearningStudies/blob/main/ObjectDetection/poringmap.jpg', cv.IMREAD_UNCHANGED)
-poring_img = cv.imread('https://github.com/jpedrocf/MachineLearningStudies/blob/main/ObjectDetection/poringclose.jpg', cv.IMREAD_UNCHANGED)
+map_img = cv.imread('https://github.com/jpedrocf/ObjectDetection-Py/blob/main/poringmap.jpg', cv.IMREAD_UNCHANGED)
+poring_img = cv.imread('https://github.com/jpedrocf/ObjectDetection-Py/blob/main/poringclose.jpg', cv.IMREAD_UNCHANGED)
 
 # dimensões da imagem
 poring_largura = poring_img.shape[1]
